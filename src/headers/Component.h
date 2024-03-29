bf6373cd63179e57bf0b4a1ef55dfcf0566231cf
SzefRon <92161300+SzefRon@users.noreply.github.com>
2024-03-29 23:11:23 +0100

    Components hold references to their GameObjects

--- src/headers/Component.h
+++ src/headers/Component.h
@@ -1,11 +1,17 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include <memory>
+
+class GameObject;
+
 class Component
 {
 protected:
     Component() = default;
 public:
+    std::shared_ptr<GameObject> game_object_;
+
     virtual ~Component() {};
     virtual void Start() = 0;
     virtual void Update() = 0;
