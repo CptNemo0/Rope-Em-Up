ac57066dff2a388baa1a5266d8a4154fe753434e
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2024-04-17 23:33:34 +0200

    HUD rendering fix

--- src/main.cc
+++ src/main.cc
@@ -401,9 +401,9 @@ int main()
 
         cubemap->RenderCube();
 
-        glDisable(GL_DEPTH_TEST);
+        /*glDisable(GL_DEPTH_TEST);
         glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);*/
 
 #pragma endregion
 #pragma region Interface
@@ -428,10 +428,10 @@ int main()
         glDisable(GL_BLEND);
         glEnable(GL_DEPTH_TEST);
 
-        BackgroundShader->Use();
+        /*BackgroundShader->Use();
         BackgroundShader->SetMatrix4("view_matrix", camera->GetViewMatrix());
 
-        cubemap->RenderCube();
+        cubemap->RenderCube();*/
 
 #pragma endregion
 
