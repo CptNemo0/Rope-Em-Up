5cc851c582468f61a5de5d17d8ad27b460ca9753
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2024-04-17 23:34:21 +0200

    and clean up

--- src/main.cc
+++ src/main.cc
@@ -401,10 +401,6 @@ int main()
 
         cubemap->RenderCube();
 
-        /*glDisable(GL_DEPTH_TEST);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);*/
-
 #pragma endregion
 #pragma region Interface
 
@@ -428,10 +424,6 @@ int main()
         glDisable(GL_BLEND);
         glEnable(GL_DEPTH_TEST);
 
-        /*BackgroundShader->Use();
-        BackgroundShader->SetMatrix4("view_matrix", camera->GetViewMatrix());
-
-        cubemap->RenderCube();*/
 
 #pragma endregion
 
