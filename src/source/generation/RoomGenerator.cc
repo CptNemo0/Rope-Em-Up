b5fdc69be24c3f09353ab6c0dd5cebdde515e2f1
Paweł Stus <pawel.j.stus@gmail.com>
2024-05-09 22:10:12 +0200

    bugfix 2?

--- src/source/generation/RoomGenerator.cc
+++ src/source/generation/RoomGenerator.cc
@@ -526,7 +526,7 @@ void generation::BuildRoom(const Room& room, RoomModels* rm, std::deque<w_ptr<Ga
         s_ptr<GameObject> clutter = GameObject::Create(scene_root);
         clutter->transform_->set_position(room.clutter_positions[i]);
         clutter->AddComponent(make_shared<components::MeshRenderer>(rm->clutter[room.clutter_idx[i]], shader));
-        clutter->AddComponent(collisions::CollisionManager::i_->CreateCollider(0, gPRECISION, rm->clutter[room.clutter_idx[i]]->meshes_[0], clutter->transform_));
+        //clutter->AddComponent(collisions::CollisionManager::i_->CreateCollider(0, gPRECISION, rm->clutter[room.clutter_idx[i]]->meshes_[0], clutter->transform_));
         room_parts.push_back(clutter);
         clutter->PropagateStart();
     }
